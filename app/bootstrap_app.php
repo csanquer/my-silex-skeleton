91dc05bee175f23a44fdda78778c90bbec29733b
Charles Sanquer <charles.sanquer@gmail.com>
2013-09-14 14:20:48 +0200

    enable twig extensions text and intl

--- app/bootstrap_app.php
+++ app/bootstrap_app.php
@@ -229,6 +229,13 @@ $app['twig'] = $app->share($app->extend('twig', function($twig, $app) use ($conf
         }
     }
     
+    // add extensions
+    $twig->addExtension(new Twig_Extensions_Extension_Text());
+    
+    if (extension_loaded('intl')) {
+        $twig->addExtension(new Twig_Extensions_Extension_Intl());
+    }
+    
     return $twig;
 }));
 
